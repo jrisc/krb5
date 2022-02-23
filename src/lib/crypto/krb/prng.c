bff86241b726563c9694e59b939ae6b3fe94c1d3
Julien Rische <jrische@redhat.com>
2022-02-23 17:34:33 +0100

    FIPS with PRNG and RADIUS and MD4
    NB: Use openssl's PRNG in FIPS mode and taint within krad.
    
    A lot of the FIPS error conditions from OpenSSL are incredibly
    mysterious (at best, things return NULL unexpectedly; at worst,
    internal assertions are tripped; most of the time, you just get
    ENOMEM).  In order to cope with this, we need to have some level of
    awareness of what we can and can't safely call.
    
    This will slow down some calls slightly (FIPS_mode() takes multiple
    locks), but not for any ciphers we care about - which is to say that
    AES is fine.  Shame about SPAKE though.
    
    post6 restores MD4 (and therefore keygen-only RC4).
    
    post7 restores MD5 and adds radius_md5_fips_override.
    
    post8 restores MD4/MD5 for OpenSSL 3.0
    
    Use OpenSSL 3.0 library context to access MD4 and MD5 lazily from
    legacy provider if RC4 encryption type is enabled, without affecting
    global context.
    
    Remove EVP_MD_CTX_FLAG_NON_FIPS_ALLOW flag since does not have any
    effect anymore.
    
    Last-updated: krb5-1.19

--- src/lib/crypto/krb/prng.c
+++ src/lib/crypto/krb/prng.c
@@ -26,6 +26,8 @@
 
 #include "crypto_int.h"
 
+#include <openssl/rand.h>
+
 krb5_error_code KRB5_CALLCONV
 krb5_c_random_seed(krb5_context context, krb5_data *data)
 {
@@ -99,9 +101,16 @@ krb5_boolean
 k5_get_os_entropy(unsigned char *buf, size_t len, int strong)
 {
     const char *device;
-#if defined(__linux__) && defined(SYS_getrandom)
     int r;
 
+    /* A wild FIPS mode appeared! */
+    if (FIPS_mode()) {
+        /* The return codes on this API are not good */
+        r = RAND_bytes(buf, len);
+        return r == 1;
+    }
+
+#if defined(__linux__) && defined(SYS_getrandom)
     while (len > 0) {
         /*
          * Pull from the /dev/urandom pool, but require it to have been seeded.
--- src/lib/crypto/openssl/hash_provider/hash_evp.c
+++ src/lib/crypto/openssl/hash_provider/hash_evp.c
@@ -32,6 +32,50 @@
 
 #include "crypto_int.h"
 #include <openssl/evp.h>
+#include <openssl/provider.h>
+#include <threads.h>
+
+typedef struct ossl_lib_md_context {
+    OSSL_LIB_CTX *libctx;
+    OSSL_PROVIDER *legacy_provider;
+    EVP_MD *md;
+} ossl_md_context_t;
+
+static thread_local ossl_md_context_t *ossl_md_ctx = NULL;
+
+static krb5_error_code
+init_ossl_md_ctx(ossl_md_context_t *ctx, const char *algo)
+{
+    ctx->libctx = OSSL_LIB_CTX_new();
+    if (!ctx->libctx)
+        return KRB5_CRYPTO_INTERNAL;
+
+    /*
+     * Load both legacy and default provider as both may be needed.
+     * If they fail keep going and an error will be raised when we try to
+     * fetch the cipher later.
+     */
+    ctx->legacy_provider = OSSL_PROVIDER_load(ctx->libctx, "legacy");
+
+    ctx->md = EVP_MD_fetch(ctx->libctx, algo, NULL);
+    if (!ctx->md)
+        return KRB5_CRYPTO_INTERNAL;
+
+    return 0;
+}
+
+static void
+deinit_ossl_ctx(ossl_md_context_t *ctx)
+{
+    if (ctx->md)
+        EVP_MD_free(ctx->md);
+
+    if (ctx->legacy_provider)
+        OSSL_PROVIDER_unload(ctx->legacy_provider);
+
+    if (ctx->libctx)
+        OSSL_LIB_CTX_free(ctx->libctx);
+}
 
 static krb5_error_code
 hash_evp(const EVP_MD *type, const krb5_crypto_iov *data, size_t num_data,
@@ -62,15 +106,52 @@ hash_evp(const EVP_MD *type, const krb5_crypto_iov *data, size_t num_data,
 }
 
 static krb5_error_code
+hash_legacy_evp(const char *algo, const krb5_crypto_iov *data, size_t num_data,
+                krb5_data *output)
+{
+    krb5_error_code err;
+
+    if (!ossl_md_ctx) {
+        ossl_md_ctx = malloc(sizeof(ossl_md_context_t));
+        if (!ossl_md_ctx)
+            return ENOMEM;
+
+        err = init_ossl_md_ctx(ossl_md_ctx, algo);
+        if (err) {
+            deinit_ossl_ctx(ossl_md_ctx);
+            free(ossl_md_ctx);
+            ossl_md_ctx = NULL;
+            goto end;
+        }
+    }
+
+    err = hash_evp(ossl_md_ctx->md, data, num_data, output);
+
+end:
+    return err;
+}
+
+static krb5_error_code
 hash_md4(const krb5_crypto_iov *data, size_t num_data, krb5_data *output)
 {
-    return hash_evp(EVP_md4(), data, num_data, output);
+    /*
+     * MD4 is needed in FIPS mode to perform key generation for RC4 keys used
+     * by IPA.  These keys are only used along a (separately) secured channel
+     * for legacy reasons when performing trusts to Active Directory.
+     */
+    return FIPS_mode() ? hash_legacy_evp("MD4", data, num_data, output)
+                       : hash_evp(EVP_md4(), data, num_data, output);
 }
 
 static krb5_error_code
 hash_md5(const krb5_crypto_iov *data, size_t num_data, krb5_data *output)
 {
-    return hash_evp(EVP_md5(), data, num_data, output);
+    /*
+     * MD5 is needed in FIPS mode for communication with RADIUS servers.  This
+     * is gated in libkrad by libdefaults->radius_md5_fips_override.
+     */
+    return FIPS_mode() ? hash_legacy_evp("MD5", data, num_data, output)
+                       : hash_evp(EVP_md5(), data, num_data, output);
 }
 
 static krb5_error_code
